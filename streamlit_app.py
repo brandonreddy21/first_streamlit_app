64af1cf58bb800f72e8ede168affb9989cc08a52
brandonreddy21 <132416375+brandonreddy21@users.noreply.github.com>
2023-11-05 20:56:21 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,9 +65,9 @@ if streamlit.button('Add a Fruit to the List'):
     streamlit.text(back_from_function)
   
 # Add a button to load the fruit 
-if streamlit.button('Get Fruit Load List'):
-  my_data_rows = get_fruit_load_list()
-  streamlit.dataframe(my_data_rows)
+#if streamlit.button('Get Fruit Load List'):
+ # my_data_rows = get_fruit_load_list()
+  #streamlit.dataframe(my_data_rows)
   
 # Don't run anything past here while we troubleshoot
 streamlit.stop()
